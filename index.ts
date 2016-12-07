931478d43cec0aaaf040e453975d4012e989f244
Mateusz Kocz <mateusz@kocz.pl>
2016-12-07 18:37:51 +0100

    feat(lib): expose style interfaces

--- index.ts
+++ index.ts
@@ -1,2 +1,4 @@
 export * from './src/background-text-color-match.directive'
 export * from './src/background-text-color-match.module'
+export * from './src/style'
+export * from './src/styles-declaration'
